cdbb545587269274fd5798a617e0ba8ecf7a5638
Preet <iam@preetpatel.com>
2018-07-22 20:58:43 +1200

    Added 2 test cases

--- src/uriparser/TestURIParser.java
+++ src/uriparser/TestURIParser.java
@@ -1,6 +1,8 @@
 package uriparser;  // DO NOT CHANGE THIS OR YOU WILL GET ZERO
 
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 /**
@@ -13,7 +15,20 @@ public class TestURIParser {// DO NOT CHANGE THE CLASS NAME OR YOU WILL GET ZERO
 
     // Your tests here.
     @Test
-    public void demo() {
-        fail("I am a demo test case. I can be deleted");
+    public void testWebUrl() {
+        URIParser parse = new URIParser();
+        URI uri = parse.parse("http://www.youtube.com");
+        assertEquals("http", uri.getScheme());
+    }
+
+    @Test
+    public void testNullInput() {
+        try {
+            new URIParser().parse(null);
+            fail();
+        }
+        catch(ParseException e) {
+            //Nothing to do here
+        }
     }
 }

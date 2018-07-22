9cefbecbc9de90421d821d7028552db81c29e343
Preet <iam@preetpatel.com>
2018-07-22 20:46:17 +1200

    Add TestSuite.java file

---
+++ src/uriparser/TestURIParser.java
@@ -0,0 +1,19 @@
+package uriparser;  // DO NOT CHANGE THIS OR YOU WILL GET ZERO
+
+import org.junit.Test;
+import static org.junit.Assert.fail;
+
+/**
+ * SOFTENG 254 2018 Assignment 1 submission
+ *
+ * Author: (name, username)
+ **/
+
+public class TestURIParser {// DO NOT CHANGE THE CLASS NAME OR YOU WILL GET ZERO
+
+    // Your tests here.
+    @Test
+    public void demo() {
+        fail("I am a demo test case. I can be deleted");
+    }
+}
